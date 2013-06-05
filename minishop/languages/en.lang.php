bd23ecd0bce31c128ca844e4ed734725f0e6871b
Moncho Varela <nakome@gmail.com>
2013-06-05 12:32:12 +0200

    Update en.lang.php

--- minishop/languages/en.lang.php
+++ minishop/languages/en.lang.php
@@ -1,10 +1,5 @@
 <?php
 
     return array(
-        'sandbox' => array(
-            'Sandbox' => 'Sandbox',
-            'Sandbox plugin for Monstra' => 'Sandbox plugin for Monstra',
-            'Sandbox template' => 'Sandbox template',
-            'Save' => 'Save',
-        )
+        'minishop' => array()
     );
